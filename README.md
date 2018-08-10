d1428d9a6b6befabc013101492c7f551a29ddade
Benjamin Gray <bengray178@gmail.com>
2018-08-10 21:12:49 +1000

    Add file

---
+++ README.md
@@ -0,0 +1,3 @@
+# tree-sitter-quick-check
+
+A grammar to test your Tree-sitter development workflow
---
+++ grammar.js
@@ -0,0 +1,18 @@
+module.exports = grammar({
+  name: "quick-check",
+
+  extras: $ => [/[\s\n\t]/, $.comment],
+
+  rules: {
+    program: $ => repeat(choice(
+      $._command_or_entry,
+      $.comment,
+      $.junk
+    )),
+
+    comment: $ => token(seq('%', /.*/)),
+    junk: $ => /[^%@\s\n\t][^%@]*/,
+
+    _command_or_entry: $ => seq("@", /[a-zA-Z]+/)
+  }
+});
